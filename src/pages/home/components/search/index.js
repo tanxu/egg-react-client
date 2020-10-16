6d0298537dd7d332789e1c70d6ec08c8f41445c9
tank.tan <309313429@qq.com>
2020-10-16 16:30:49 +0800

    首页开发

---
+++ src/pages/home/components/search/index.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Picker, List, Calendar,Button } from 'antd-mobile';
+import dayjs from 'dayjs';
+
+export default function(props) {
+  const [citys, setCitys] = useState([
+    [{ label: '杭州', value: '10001' }, { label: '苏州', value: '10002' }],
+  ]);
+
+  const [selectedCity, setSelectedCity] = useState(['10001']);
+  const [times, setTimes] = useState('可选时间');
+  const [dateShow, setDateShow] = useState(false);
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleCityChange = (value) => {
+    console.log('va', value);
+    setSelectedCity(value);
+  };
+  const handleDate = () => {
+    setDateShow(!dateShow);
+  };
+  const handleDateConfim = (startTime, endTime) => {
+    setTimes(dayjs(startTime).format('YYYY-MM-DD') + '~' + dayjs(endTime).format('YYYY-MM-DD'));
+    handleDate();
+  };
+
+
+  return (
+    <div className='search'>
+      {/*可选城市*/}
+      <div className="search-addr">
+        <Picker title='城市' data={citys} value={selectedCity} cascade={false} cols={1}
+                onChange={handleCityChange}>
+          <List.Item>可选城市</List.Item>
+        </Picker>
+      </div>
+      {/*可选时间*/}
+      <div className="search-time" onClick={handleDate}>
+        <p className='search-time_left'>出租时间</p>
+        <p className="search-time_right">{times}</p>
+      </div>
+      {/*点击按钮*/}
+      <Button type='warning' size='large'>搜索民宿</Button>
+
+      <Calendar visible={dateShow} onCancel={handleDate} onConfirm={handleDateConfim}></Calendar>
+    </div>
+  );
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import Header from './components/header';
+import Search from './components/search';
+import Hot from './components/hot';
+import './index.less';
 
 export default function(props) {
   const [state, setState] = useState();
@@ -8,8 +12,13 @@ export default function(props) {
   }, []);
 
   return (
-    <div>
-      home
+    <div className='home'>
+      {/*header区域*/}
+      <Header />
+      {/*search区域*/}
+      <Search />
+      {/*hot区域*/}
+      <Hot />
     </div>
   );
 }
