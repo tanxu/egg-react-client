9a7a06ca8cf6ccc1bde671807c98f34e3c1508b7
tank.tan <309313429@qq.com>
2020-10-22 16:18:14 +0800

    个人中心页面 增加fc-form插件

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { history } from 'umi';
+import { List } from 'antd-mobile';
+import './index.less';
 
 export default function(props) {
   const [state, setState] = useState();
@@ -6,10 +9,33 @@ export default function(props) {
   useEffect(() => {
 
   }, []);
-
+  const handleClick = () => {
+    history.push({
+      pathname: '/user/edit',
+      query: {
+        id: 10,
+      },
+    });
+  };
   return (
-    <div>
-      user
+    <div className={'user-page'}>
+      {/*用户信息*/}
+      <div className={'info'}>
+        <div className="set" onClick={handleClick}>设置</div>
+        <div className="user">
+          <img src="" alt="" />
+          <div className="tel">tel</div>
+          <div className="sign">sign</div>
+        </div>
+      </div>
+      {/*列表*/}
+      <div className="lists">
+        <List>
+          <List.Item arrow={'horizontal'}>用户协议</List.Item>
+          <List.Item arrow={'horizontal'}>常见问题</List.Item>
+          <List.Item arrow={'horizontal'}>联系客服</List.Item>
+        </List>
+      </div>
     </div>
   );
 }
